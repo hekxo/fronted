299bc7dec4e7d557b0fbce3f8431a1b1b83ba7f7
hekxo <raminsagandykov@gmail.com>
2023-11-24 05:40:30 +0600

    Initial Commit

--- myScript.js
+++ myScript.js
@@ -5,6 +5,21 @@ document.getElementById("play-button").addEventListener("click", function() {
 });
 
 
+let myButton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 500) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+}
+
 
 
 // Create a "close" button and append it to each list item
@@ -63,25 +78,133 @@ function newElement() {
 }
 
 
-// Add an event listener to the form with the id "myForm"
-    document.getElementById("myForm").addEventListener("submit", function (event) {
-        // Get the email input element and its value
-        const emailInput = document.getElementById("email");
-        const emailValue = emailInput.value;
+document.addEventListener('DOMContentLoaded', function () {
+    const video = document.getElementById('fullscreen-video');
+    video.webkitRequestFullscreen = undefined;
+    video.mozRequestFullScreen = undefined;
+    const fullscreenButton = document.getElementById('fullscreen-button');
 
-        // Regular expression to validate email format
-        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-        // Check if the email value matches the regular expression
-            if (!emailRegex.test(emailValue)) {
-                // If it doesn't match, show an alert and prevent form submission
-                alert("Please try again.");
-                event.preventDefault(); // Prevent the form from submitting
-            }
-            else {
-                alert("Successfully logged in!");
-            }
+    fullscreenButton.addEventListener('click', function () {
+        if (video.requestFullscreen) {
+            video.requestFullscreen();
+        } else if (video.mozRequestFullScreen) {
+            video.mozRequestFullScreen();
+        } else if (video.webkitRequestFullscreen) {
+            video.webkitRequestFullscreen();
+        }
     });
+});
+
+
+document.getElementById("fullscreen-button1").addEventListener("click", function () {
+    const video1 = document.getElementById("fullscreen-video1");
+    if (video1.style.display === "none") {
+        video1.style.display = "block";
+    } else {
+        video1.style.display = "none";
+    }
+})
+document.getElementById("fullscreen-button2").addEventListener("click", function () {
+    const video2 = document.getElementById("fullscreen-video2");
+    if (video2.style.display === "none") {
+        video2.style.display = "block";
+    } else {
+        video2.style.display = "none";
+    }
+})
+document.getElementById("fullscreen-button3").addEventListener("click", function () {
+    const video3 = document.getElementById("fullscreen-video3");
+    if (video3.style.display === "none") {
+        video3.style.display = "block";
+    } else {
+        video3.style.display = "none";
+    }
+})
+
+
+
+// // Add an event listener to the form with the id "myForm"
+//     document.getElementById("myForm").addEventListener("submit", function (event) {
+//         // Get the email input element and its value
+//         const emailInput = document.getElementById("email");
+//         const emailValue = emailInput.value;
+//
+//         // Regular expression to validate email format
+//         const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//
+//         // Check if the email value matches the regular expression
+//             if (!emailRegex.test(emailValue)) {
+//                 // If it doesn't match, show an alert and prevent form submission
+//                 alert("Please try again.");
+//                 event.preventDefault(); // Prevent the form from submitting
+//             }
+//             else {
+//                 alert("Successfully logged in!");
+//             }
+//     });
+
+
+
+function submitQuiz() {
+    const answers = {
+        q1: document.querySelector('input[name="q1"]:checked'),
+        q2: document.querySelector('input[name="q2"]:checked'),
+        q3: document.querySelector('input[name="q3"]:checked'),
+        q4: document.querySelector('input[name="q4"]:checked'),
+        q5: document.querySelector('input[name="q5"]:checked'),
+        q6: document.querySelector('input[name="q6"]:checked'),
+        q7: document.querySelector('input[name="q7"]:checked'),
+        q8: document.querySelector('input[name="q8"]:checked'),
+        q9: document.querySelector('input[name="q9"]:checked'),
+        q10: document.querySelector('input[name="q10"]:checked')
+    };
+
+    let score = 0;
+
+    if (answers.q1 && answers.q1.value === "a") {
+        score++;
+    }
+
+    if (answers.q2 && answers.q2.value === "b") {
+        score++;
+    }
+
+    if (answers.q3 && answers.q3.value === "a") {
+        score++;
+    }
+
+    if (answers.q4 && answers.q4.value === "c") {
+        score++;
+    }
+
+    if (answers.q5 && answers.q5.value === "a") {
+        score++;
+    }
+
+    if (answers.q6 && answers.q6.value === "b") {
+        score++;
+    }
+
+    if (answers.q7 && answers.q7.value === "c") {
+        score++;
+    }
+
+    if (answers.q8 && answers.q8.value === "b") {
+        score++;
+    }
+
+    if (answers.q9 && answers.q9.value === "c") {
+        score++;
+    }
+
+    if (answers.q10 && answers.q10.value === "c") {
+        score++;
+    }
+
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `Your score is: ${score} out of 10`;
+}
+
 
 // Set the date and time for the new season
 const newSeasonDate = new Date('2023-12-01T00:00:00').getTime();
